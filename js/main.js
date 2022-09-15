c0dbf82e20546f4841e8667f6100f51f23aaa70b
Liz Manresa <manresa.liz@gmail.com>
2022-09-15 17:13:57 +0200

    create and move obstacles

--- js/main.js
+++ js/main.js
@@ -1,38 +1,49 @@
+
 class Game {
     constructor(){
-        this.player = null;
+        this.player = null; //will store an instance of the class Player
+        this.obstacles = []; //will store instances of the class Obstacle
     }
 
     start(){
-        console.log("starting game");
         this.player = new Player();
         this.attachEventListeners();
-    }
+        
+        //move obstacles
+        setInterval(() => {
+            this.obstacles.forEach( (obstacleInstance) => {
+                obstacleInstance.moveDown();
+            });
+        }, 60);
 
+        //create new obstacles
+        setInterval(() => {
+            const newObstacle = new Obstacle();
+            this.obstacles.push(newObstacle);
+        }, 3000);
+    }
     attachEventListeners(){
         document.addEventListener("keydown", (event) => {
             if(event.key === "ArrowLeft"){
                 this.player.moveLeft();
-            }else if (event.key === "ArrowRight") {
+            }else if(event.key === "ArrowRight"){
                 this.player.moveRight();
             }
         });
     }
- 
-
 }
+
 
 class Player {
     constructor(){
         this.positionX = 50;
         this.positionY = 0;
         this.width = 20;
-        this.height = 20;
+        this.height = 10;
         this.domElement = null;
 
         this.createDomElement();
     }
-
     createDomElement(){
         // create dom element
         this.domElement = document.createElement('div');
@@ -45,22 +56,48 @@ class Player {
         this.domElement.style.left = this.positionX + "vw";
 
         // append to the dom
-       const boardElm = document.getElementById("board");
-       boardElm.appendChild(this.domElement)
-
-        
+        const boardElm = document.getElementById("board");
+        boardElm.appendChild(this.domElement)
     }
-
     moveLeft(){
-        console.log("moving left...",this.positionX);
-      this.positionX--;
-      this.domElement.style.left = this.positionX + "vw";
+        this.positionX--;
+        this.domElement.style.left = this.positionX + "vw";
     }
-
     moveRight(){
-        console.log("moving right...", this.positionX);
         this.positionX++;
         this.domElement.style.left = this.positionX + "vw";
+    }
+}
+
+
+class Obstacle {
+    constructor(){
+        this.positionX = 50;
+        this.positionY = 90;
+        this.width = 10;
+        this.height = 10;
+        this.domElement = null;
+
+        this.createDomElement();
+    }
+    createDomElement(){
+        // create dom element
+        this.domElement = document.createElement('div');
+
+        // set id and css
+        this.domElement.className = "obstacle";
+        this.domElement.style.width = this.width + "vw";
+        this.domElement.style.height = this.height + "vh";
+        this.domElement.style.bottom = this.positionY + "vh";
+        this.domElement.style.left = this.positionX + "vw";
+
+        // append to the dom
+        const boardElm = document.getElementById("board");
+        boardElm.appendChild(this.domElement)
+    }
+    moveDown(){
+        this.positionY--;
+        this.domElement.style.bottom = this.positionY + "vh";
     }
 }
 
